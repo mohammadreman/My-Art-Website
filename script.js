a429ea8ec87bbdd4be4425bac82e8d83a7d69324
mohammad reman <58370316+mohammadreman@users.noreply.github.com>
2022-02-20 20:19:36 +0600

    i'm done

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ $(function() {
         var $nav = $(".navbar");
         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
+
+    // toggle menu/ navbar script
+    $('.menu-btn').click(function() {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
 });
